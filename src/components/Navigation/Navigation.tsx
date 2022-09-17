2d6be24f3dbee456d99c4bd9db9e0d4f857c6786
CHIDUBEM OKAFOR <okaforchidubem7@gmail.com>
2022-09-17 17:40:20 +0100

    style

--- src/components/Navigation/Navigation.tsx
+++ src/components/Navigation/Navigation.tsx
@@ -1,24 +1,19 @@
 import { FC } from "react";
 
 const Navigation: FC = () => {
-
   return (
-    <>
-      <nav className="flex justify-center w-full border-2 border-red-300 h-20">
+    <div>
+      <nav className="flex justify-center bg-zinc-900 w-full  h-20 border-indigo-500">
+        <div className="flex items-center w-3/4 justify-between ">
+          <div className="">dLotto logo</div>
 
-        <div className="flex items-center w-3/4 border-2 border-blue-300">
-          <div className="border-2 border-red-300">
-            dLotto logo
-          </div>
-
-          <button>
+          <button className="text-white bg-gradient-to-r from-blue-500 via-blue-600 to-blue-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 shadow-lg shadow-blue-500/50 dark:shadow-lg dark:shadow-blue-800/80 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2 ">
             Connect wallet
           </button>
         </div>
-
       </nav>
-    </>
-  )
-}
+    </div>
+  );
+};
 
 export default Navigation;
