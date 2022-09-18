9b59146a41c0c3b196c3425bac4c271f0c14d595
BlackH3art <jaroslaw.musielak@outlook.com>
2022-09-18 08:47:29 +0200

    feature: buy ticket + ToastContainer

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,6 @@
+import 'react-toastify/dist/ReactToastify.css';
+import { ToastContainer } from 'react-toastify';
+
 import Navigation from "./components/Navigation/Navigation";
 import Main from "./components/Main/Main";
 import TicketSection from "./components/TicketSection/TicketSection";
@@ -6,6 +9,7 @@ import GameRules from "./components/GameRules/GameRules";
 function App() {
   return (
     <>
+      <ToastContainer />
       <Navigation />
       <Main />
       <TicketSection />
--- src/components/TicketSection/NumbersForm.tsx
+++ src/components/TicketSection/NumbersForm.tsx
@@ -1,17 +1,18 @@
-import { ChangeEvent, FC, useState, ChangeEventHandler } from "react";
+import { ChangeEvent, FC, useState, ChangeEventHandler, FormEventHandler, FormEvent, useContext } from "react";
+import { ethers } from "ethers";
+
+import { AppContext } from "../../context/AppContext";
+
 import NumberButton from "./NumberButton";
+import { toast } from "react-toastify";
 
 const NumbersForm: FC = () => {
-  const rangeArray = [
-    1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21,
-    22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36,
-  ];
+  const rangeArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36];
 
   const [selectedNumbers, setSelectedNumbers] = useState<number[]>([]);
+  const { contractSigner } = useContext(AppContext);
 
-  const handleChange: ChangeEventHandler = (
-    e: ChangeEvent<HTMLInputElement>
-  ) => {
+  const handleChange: ChangeEventHandler = (e: ChangeEvent<HTMLInputElement>) => {
     let val = Number(e.target.value);
 
     if (selectedNumbers.includes(val)) {
@@ -22,6 +23,35 @@ const NumbersForm: FC = () => {
 
     setSelectedNumbers((prev) => [...prev, val]);
   };
+
+  const handleSubmit: FormEventHandler = async (e: FormEvent<HTMLInputElement>) => {
+    e.preventDefault();
+
+    try {
+
+      if(selectedNumbers.length === 6) {
+
+        const txHash = await contractSigner?.buyTicket(
+          selectedNumbers[0],
+          selectedNumbers[1],
+          selectedNumbers[2],
+          selectedNumbers[3],
+          selectedNumbers[4],
+          selectedNumbers[5],
+          { value: ethers.utils.parseEther("0.01")._hex }
+        );
+
+        await txHash.wait()
+      } else {
+        toast.error("You need to select 6 numbers", { theme: "colored" });
+      }
+
+    } catch (error) {
+      console.log(error);
+      toast.error("Something went wrong", { theme: "colored" });
+    }
+
+  }
 
   return (
     <>
@@ -53,7 +83,10 @@ const NumbersForm: FC = () => {
         </div>
 
         <div className="flex justify-center pt-10">
-          <button className="text-white bg-gradient-to-r from-red-400 via-red-500 to-red-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-red-300 dark:focus:ring-red-800 shadow-lg shadow-red-500/50 dark:shadow-lg dark:shadow-red-800/80 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2">
+          <button 
+            className="text-white bg-gradient-to-r from-red-400 via-red-500 to-red-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-red-300 dark:focus:ring-red-800 shadow-lg shadow-red-500/50 dark:shadow-lg dark:shadow-red-800/80 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2"
+            onClick={handleSubmit}
+          >
             Buy ticket
           </button>
         </div>
